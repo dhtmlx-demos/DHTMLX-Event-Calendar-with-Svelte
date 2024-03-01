c857f7a452794f2e7c0bbcc9953f70a3c3d270ff
DHTMLX <155955527+dhtmlx1@users.noreply.github.com>
2024-03-01 03:26:54 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script>
+  import Calendar from "./Calendar.svelte";
+  import { getData } from "./data.js";
+</script>
+
+<Calendar events={getData()} date={new Date(2021, 5, 10)} />
---
+++ src/Calendar.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from "svelte";
+    
+    import { EventCalendar } from "@dhx/trial-eventcalendar";
+    import "@dhx/trial-eventcalendar/dist/event-calendar.css";
+    
+    export let events;
+    export let date;
+
+    let container;
+    onMount(() => {
+        new EventCalendar(container, {
+            events, date
+        })
+    });
+</script>
+
+<div bind:this={container} style="width: 100%; height: 100%;"></div>
+
